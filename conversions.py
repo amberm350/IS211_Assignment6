2ec25a5e676748d5c6f4f6b1cdcaeaff964b9d48
amberm350 <112363236+amberm350@users.noreply.github.com>
2022-10-02 16:49:17 -0400

    Add files via upload

--- conversions.py
+++ conversions.py
@@ -1,9 +1,29 @@
 
 def convertCelsiusToKelvin(celsius):
     kelvin = celsius + 273.15
-    return 0.0
-
+    return kelvin
+    
 def convertCelsiusToFahrenheit(celsius):
-    fahrenheit = (celsius * 9/5) +32
-    return 0.0
+    fahrenheit = (celsius * 9/5) + 32
+    return fahrenheit
+    
+def convertFahrenheitToCelsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+def convertFahrenheitToKelvin(fahrenheit):
+    kelvin = ((fahrenheit - 32) * 5/9) + 273.15
+    return kelvin
+
+def convertKelvinToCelsius(kelvin):
+    celsius = kelvin - 273.15
+    return celsius
+
+def convertKelvinToFahrenheit(kelvin):
+    fahrenheit = ((kelvin - 273.15) * 9/5) + 32
+    return fahrenheit 
+
+def converMilesToMeters(miles):
+    result = 0
+    return result
     
